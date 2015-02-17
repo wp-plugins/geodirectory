b4daabc80d8b0484cfb1e76efc3e8a70cf64da20
paoltaia <paoltaia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-17 21:35:39 +0000

    tagging version 1.3.8
    git-svn-id: https://plugins.svn.wordpress.org/geodirectory/tags/1.3.8@1092796 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geodirectory.php
+++ geodirectory.php
@@ -3,23 +3,49 @@
 Plugin Name: GeoDirectory
 Plugin URI: http://wpgeodirectory.com/
 Description: GeoDirectory plugin for wordpress.
-Version: 1.2.4
+Version: 1.3.8
 Author: GeoDirectory
 Author URI: http://wpgeodirectory.com
 Requires at least: 3.1
-Tested up to: 4.0
+Tested up to: 4.1
 */
 
-
-define("GEODIRECTORY_VERSION", "1.2.4");
+define("GEODIRECTORY_VERSION", "1.3.8");
 
 if (!session_id()) session_start();
+
+///CHECK FOR OLD COMPATIBILITY PACKS AND DISABLE IF THEY ARE ACTIVE
+if(is_admin()){
+include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+
+if(is_plugin_active('geodirectory-genesis-compatibility-pack/geodir_genesis_compatibility.php')){
+deactivate_plugins('geodirectory-genesis-compatibility-pack/geodir_genesis_compatibility.php');
+}
+
+if(is_plugin_active('geodirectory-x-theme-compatibility-pack/geodir_x_compatibility.php')){
+deactivate_plugins('geodirectory-x-theme-compatibility-pack/geodir_x_compatibility.php'); 
+}
+
+if(is_plugin_active('geodirectory-enfold-theme-compatibility-pack/geodir_enfold_compatibility.php')){
+deactivate_plugins('geodirectory-enfold-theme-compatibility-pack/geodir_enfold_compatibility.php'); 
+}
+
+if(is_plugin_active('geodir_avada_compatibility/geodir_avada_compatibility.php')){
+deactivate_plugins('geodir_avada_compatibility/geodir_avada_compatibility.php'); 
+}
+
+if(is_plugin_active('geodir_compat_pack_divi/geodir_divi_compatibility.php')){
+deactivate_plugins('geodir_compat_pack_divi/geodir_divi_compatibility.php'); 
+}
+
+}
+
 
 /**
  * Global
  **/ 
 global $wpdb,$plugin_prefix,$geodir_addon_list, $plugin_file_name;
-$plugin_prefix = 'geodir_';
+$plugin_prefix = $wpdb->prefix.'geodir_';
 
 $plugin_file_name = basename(plugin_dir_path(__FILE__)). '/' . basename(__FILE__);
 /**
@@ -33,6 +59,7 @@ $geodir_post_custom_fields_cache = array();// This will store the cached post cu
 // depreciated //if (!defined('GEODIRECTORY_TEMPLATE_URL')) define('GEODIRECTORY_TEMPLATE_URL', 'geodirectory/');	
 
 /* ---- Table Names ---- */
+
 if (!defined('GEODIR_COUNTRIES_TABLE')) define('GEODIR_COUNTRIES_TABLE', $plugin_prefix . 'countries' );	
 //if (!defined('GEODIR_PRICE_TABLE')) define('GEODIR_PRICE_TABLE', $plugin_prefix . 'price' );	
 //if (!defined('GEODIR_INVOICE_TABLE')) define('GEODIR_INVOICE_TABLE', $plugin_prefix . 'invoice' );	
@@ -40,8 +67,13 @@ if (!defined('GEODIR_CUSTOM_FIELDS_TABLE')) define('GEODIR_CUSTOM_FIELDS_TABLE',
 if (!defined('GEODIR_ICON_TABLE')) define('GEODIR_ICON_TABLE', $plugin_prefix . 'post_icon' );	
 if (!defined('GEODIR_ATTACHMENT_TABLE')) define('GEODIR_ATTACHMENT_TABLE', $plugin_prefix . 'attachments' );	
 if (!defined('GEODIR_REVIEW_TABLE')) define('GEODIR_REVIEW_TABLE', $plugin_prefix . 'post_review' );	
-if (!defined('GEODIR_CUSTOM_SORT_FIELDS_TABLE')) define('GEODIR_CUSTOM_SORT_FIELDS_TABLE', $plugin_prefix . 'custom_sort_fields' );	
+if (!defined('GEODIR_CUSTOM_SORT_FIELDS_TABLE')) define('GEODIR_CUSTOM_SORT_FIELDS_TABLE', $plugin_prefix . 'custom_sort_fields' );
 
+
+// disable cache on homepage
+if($_SERVER['REQUEST_URI']=='' || $_SERVER['REQUEST_URI']=='/'){
+define('DONOTCACHEPAGE', TRUE);
+}
 /**
  * Localisation
  **/
